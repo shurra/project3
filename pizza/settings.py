8508d9ce88529c8b12bfabc04f9f340c3623f30f
Roman Shkromyda <shroman@ukr.net>
2018-12-24 14:47:59 +0200

    django-heroku package added

--- pizza/settings.py
+++ pizza/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,3 +131,5 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
