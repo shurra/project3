49c6a9c79ff9896ea83af3321bd8d362aafa6314
Roman Shkromyda <shroman@ukr.net>
2018-12-26 16:22:02 +0200

    Debug mode off

--- pizza/settings.py
+++ pizza/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i0&iq&e9u9h6(4_7%pt2s9)f=c$kso=k$c$w@fi9215s=1q0^d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['shroman-project3.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['shroman-pizza-final.herokuapp.com', '127.0.0.1']
 
 INTERNAL_IPS = ['127.0.0.1']
 
